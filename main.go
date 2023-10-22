4f308c222a88d781a246ce75b9cb5a263a8083c1
Randi Dochot <dev@dochot.be>
2023-10-22 16:50:28 +0200

    add method POST,PUT,GET,DELETE for schedule

--- main.go
+++ main.go
@@ -45,12 +45,12 @@ type HolidayUser struct {
 var db *gorm.DB
 var err error
 
+// function for user
 func getUser(c *gin.Context) {
 	var user []User
 	db.Find(&user)
 	c.IndentedJSON(http.StatusOK, user)
 }
-
 func postUser(c *gin.Context) {
 	var newUser User
 	if err := c.BindJSON(&newUser); err != nil {
@@ -60,11 +60,100 @@ func postUser(c *gin.Context) {
 	db.Create(&newUser)
 	c.IndentedJSON(http.StatusCreated, newUser)
 }
-
 func deleteUser(c *gin.Context) {
 	id := c.Param("id")
 	db.Delete(&User{}, id)
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "user deleted"})
+}
+
+// function for plan schedule
+func getPlanSchedule(c *gin.Context) {
+	var planSchedule []PlanSchedule
+	db.Find(&planSchedule)
+	c.IndentedJSON(http.StatusOK, planSchedule)
+}
+func postPlanSchedule(c *gin.Context) {
+	var newPlanSchedule PlanSchedule
+	if err := c.BindJSON(&newPlanSchedule); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "error to add plan schedule"})
+		return
+	}
+	db.Create(&newPlanSchedule)
+	c.IndentedJSON(http.StatusCreated, newPlanSchedule)
+}
+func getPlanScheduleByDate(c *gin.Context) {
+	dateStart := c.Param("start")
+	dateEnd := c.Param("end")
+	var planSchedule []PlanSchedule
+	db.Where("date >= ? AND date <= ?", dateStart, dateEnd).Find(&planSchedule)
+	c.IndentedJSON(http.StatusOK, planSchedule)
+}
+func updatePlanSchedule(c *gin.Context) {
+	var planSchedule PlanSchedule
+	if err := c.BindJSON(&planSchedule); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "error to update plan schedule"})
+		return
+	}
+	db.Save(&planSchedule)
+	c.IndentedJSON(http.StatusOK, planSchedule)
+}
+
+// function for holiday user
+func getHolidayUser(c *gin.Context) {
+	var holidayUser []HolidayUser
+	db.Find(&holidayUser)
+	c.IndentedJSON(http.StatusOK, holidayUser)
+}
+func postHolidayUser(c *gin.Context) {
+	var newHolidayUser HolidayUser
+	if err := c.BindJSON(&newHolidayUser); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "error to add holiday user"})
+		return
+	}
+	db.Create(&newHolidayUser)
+	c.IndentedJSON(http.StatusCreated, newHolidayUser)
+}
+func getHolidayByUser(c *gin.Context) {
+	id := c.Param("id")
+	var holidayUser []HolidayUser
+	db.Where("user_id = ?", id).Find(&holidayUser)
+	c.IndentedJSON(http.StatusOK, holidayUser)
+}
+func removeHolidayUser(c *gin.Context) {
+	id := c.Param("id")
+	db.Delete(&HolidayUser{}, id)
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "holiday deleted"})
+}
+
+// function for plan time
+func getPlanTime(c *gin.Context) {
+	var planTime []PlanTime
+	db.Find(&planTime)
+	c.IndentedJSON(http.StatusOK, planTime)
+}
+func postPlanTime(c *gin.Context) {
+	var newPlanTime PlanTime
+	if err := c.BindJSON(&newPlanTime); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "error to add plan time"})
+		return
+	}
+	db.Create(&newPlanTime)
+	c.IndentedJSON(http.StatusCreated, newPlanTime)
+}
+func updatePlanTime(c *gin.Context) {
+	var planTime PlanTime
+	if err := c.BindJSON(&planTime); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "error to update plan time"})
+		return
+	}
+	db.Save(&planTime)
+	c.IndentedJSON(http.StatusOK, planTime)
+}
+func getPlanTimeByUser(c *gin.Context) {
+	id := c.Param("id")
+	var planTime []PlanTime
+	db.Where("user_id = ?", id).Find(&planTime)
+	c.IndentedJSON(http.StatusOK, planTime)
 }
 
 func main() {
@@ -91,9 +180,25 @@ func main() {
 	}
 
 	route := gin.Default()
+
 	route.GET("/user", getUser)
 	route.POST("/user", postUser)
 	route.DELETE("/user/:id", deleteUser)
+
+	route.GET("/planSchedule", getPlanSchedule)
+	route.POST("/planSchedule", postPlanSchedule)
+	route.GET("/planSchedule/:start/:end", getPlanScheduleByDate)
+	route.PUT("/planSchedule", updatePlanSchedule)
+
+	route.GET("/holidayUser", getHolidayUser)
+	route.POST("/holidayUser", postHolidayUser)
+	route.GET("/holidayUser/:id", getHolidayByUser)
+	route.DELETE("/holidayUser/:id", removeHolidayUser)
+
+	route.GET("/planTime", getPlanTime)
+	route.POST("/planTime", postPlanTime)
+	route.PUT("/planTime", updatePlanTime)
+	route.GET("/planTime/:id", getPlanTimeByUser)
 
 	route.Run(":8080")
 }
